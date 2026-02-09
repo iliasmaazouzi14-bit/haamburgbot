f6b40fa40f441d2d21c044f8258068ff883230b4
iliasmaazouzi14-bit <ilias.maazouzi14@gmail.com>
2026-02-09 22:58:36 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const client = new Client({
+  intents: [GatewayIntentBits.Guilds]
+});
+
+client.once("ready", () => {
+  console.log(`✅ Eingeloggt als ${client.user.tag}`);
+});
+
+client.login(process.env.DISCORD_TOKEN);
